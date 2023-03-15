465843e22ae9ef5bcf8f6a86bf4f26726e80b0a9
maguitaria <t2glma00@students.oamk.fi>
2023-03-15 14:46:11 +0200

    weirdness

--- server/index.js
+++ server/index.js
@@ -8,8 +8,23 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
-const port = 3001;
+const port = 3301;
 app.get('/', (req, res) => {
-    res.status(200).json({ result: 'success' });
+    //  const pool = openDb();
+    //  pool.query('select * from task', (error,result)=> {
+    //     if (error) {
+    //         res.status(500).json({error: error.message})
+    //     }
+    res.status(200).json({ result: "success" });
 });
 app.listen(port);
+// const openDb = (): Pool => {
+//     const pool: Pool = new Pool ({
+//         user: 'postgres',
+//         host: 'localhost',
+//         database: 'todo',
+//         password: 'root',
+//         port: 5342
+//     });
+//     return pool;
+// }
--- server/index.ts
+++ server/index.ts
@@ -5,10 +5,27 @@ import { Pool  } from 'pg';
 
 const app: Express = express();
 app.use(cors());
-
-const port = 3001;
+const port = 3301;
 
 app.get('/', (req: Request, res:Response)=> {
-    res.status(200).json({result:'success'});
-});
- app.listen(port);
+//  const pool = openDb();
+//  pool.query('select * from task', (error,result)=> {
+//     if (error) {
+//         res.status(500).json({error: error.message})
+//     }
+    res.status(200).json({result:"success"});
+
+ })
+
+app.listen(port);
+
+// const openDb = (): Pool => {
+//     const pool: Pool = new Pool ({
+//         user: 'postgres',
+//         host: 'localhost',
+//         database: 'todo',
+//         password: 'root',
+//         port: 5342
+//     });
+//     return pool;
+// }
